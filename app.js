cc59ed218136570128f2e83c5b559c3fc55174b3
Samiun Jahan <samiunjahan06@gmail.com>
2021-09-25 20:50:15 +0600

    second commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const countDownDate = new Date("Jan 1, 2027 00:00:00").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element
+  const daysContainer = document.getElementById("days")
+  const hoursContainer = document.getElementById("hours")
+  const minutesContainer = document.getElementById("minutes")
+  const secondsContainer = document.getElementById("seconds")
+
+  daysContainer.innerText = days
+  hoursContainer.innerText = hours
+  minutesContainer.innerText = minutes
+  secondsContainer.innerText = seconds
+  
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
